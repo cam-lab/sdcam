2803063eba119c11167b873def66b6d370704fac
hz <harry.zhurov@gmail.com>
2024-04-07 16:34:07 +0700

    improve log launch script: allow specify arguments

--- log.sh
+++ log.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-multitail --config .config/multitail.conf -cS pylogger log/sdcam.log -cS spdlogger log/vframe.log
+if [[ $1 == "py" ]]; then
+    multitail --config .config/multitail.conf -cS pylogger log/sdcam.log
+
+elif [[ $1 == "cpp" ]]; then
+    multitail --config .config/multitail.conf -cS spdlogger log/vframe.log
+else
+    multitail --config .config/multitail.conf -cS pylogger log/sdcam.log -cS spdlogger log/vframe.log
+fi
+
