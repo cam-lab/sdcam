7b9c1f5c289143e517df2712b3554c4463f860b5
hz <harry.zhurov@gmail.com>
2024-10-31 13:24:20 +0700

    add receive functionality for udptest

--- src/utils/udptest.cpp
+++ src/utils/udptest.cpp
@@ -63,20 +63,31 @@
 #include <vframe.h>
 #include <progressbar.h>
 
-const char     *SOCKET_IP   = "192.168.10.10";
-const uint16_t  SRC_PORT = 50000;
-const uint16_t  DST_PORT = 50000;
+const char     *SOCKET_IP      = "192.168.10.1";
+const char     *DEV_IP         = "192.168.10.10";
+const uint16_t  SRC_PORT       = 50000;
+const uint16_t  DST_PORT       = 50000;
+const size_t    PKT_SIZE       = 2048;
+const uint16_t  LAN_AGENT_IDX  = 0;
+
+using SockBuf    = std::array<uint16_t, PKT_SIZE/sizeof(uint16_t)>;
 
 //------------------------------------------------------------------------------
 
 static auto lg = spdlog::basic_logger_mt("udptest", "log/udptest.log");
 
 void udp_tx(size_t count);
+void udp_rx();
+void dump(uint8_t *p, size_t size);
 
+std::atomic_bool udp_rx_thread_start;
+std::atomic_bool udp_rx_thread_socket_fail;
 //------------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
+    lg->flush_on(spdlog::level::debug);
     lg->set_pattern("%Y-%m-%d %H:%M:%S %n   %L : %v");
+    lg->info("-------------------------");
     
     if(argc > 2)
     {
@@ -93,12 +104,27 @@ int main(int argc, char *argv[])
     try
     {
         print("------------------------------------");
+        udp_rx_thread_start.store(false);
+        udp_rx_thread_socket_fail.store(false);
+        auto rx_thread = new std::thread(udp_rx);
+        while(!udp_rx_thread_start)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            if(udp_rx_thread_socket_fail)
+            {
+                rx_thread->join();
+                return -1;
+            }
+        }
+        
         print("begin packet stream generating for {} packets\n", pkt_count);
-        //auto pkt_num = fgen(frame_count);
         udp_tx(pkt_count);
-        //print("\nend frame stream generating, {} packets sent", pkt_num);
-        print("------------------------------------");
 
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        udp_rx_thread_start.store(false);
+        rx_thread->join();
+        print("------------------------------------");
     }
     catch(SocketException e)
     {
@@ -116,10 +142,14 @@ void udp_tx(size_t count)
     uint16_t idx  = 0;
     uint32_t pnum = 0;
 
-    const uint16_t LAN_AGENT_IDX  = 0;
     const size_t PKT_PAYLOAD_SIZE = 16; // 1472/2;
 
-    std::array<uint16_t, PKT_PAYLOAD_SIZE> buf;
+    SockBuf buf;
+    struct sockaddr_in dst_addr;
+    
+    dst_addr.sin_family      = AF_INET;
+    dst_addr.sin_addr.s_addr = inet_addr(DEV_IP);
+    dst_addr.sin_port        = htons(DST_PORT);
 
     for(size_t p = 0; p < count; ++p)
     {
@@ -132,10 +162,47 @@ void udp_tx(size_t count)
             buf[i] = ((pnum+(i-idx)*2) & 0xff) + ((pnum+(i-idx)*2+1) << 8);
         }
         idx = 0;
-        
-        sock.write(reinterpret_cast<uint8_t *>(buf.data()), PKT_PAYLOAD_SIZE*2);
-    }
 
+        sock.write(dst_addr, reinterpret_cast<uint8_t *>(buf.data()), PKT_PAYLOAD_SIZE*2);
+    }
+}
+//------------------------------------------------------------------------------
+void udp_rx()
+{
+    try
+    {
+        Socket sock(lg, SOCKET_IP, DST_PORT);
+
+        sock.set_recv_timeout(std::chrono::milliseconds(500));
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        udp_rx_thread_start.store(true);
+        lg->info("create and bind Rx socket");
+
+        SockBuf pkt;
+        uint32_t id = 1;
+        for(;;)
+        {
+            int count;
+            count = sock.read(reinterpret_cast<uint8_t *>(pkt.data()), PKT_SIZE);
+            if(count >= 0)
+            {
+                dump(reinterpret_cast<uint8_t *>(pkt.data()), count);
+            }
+            
+            if(!udp_rx_thread_start)
+            {
+                return;
+            }
+        }
+    }
+    catch(SocketException e)
+    {
+        lg->error("{}, socket fd: {}", e.msg, e.fd);
+        udp_rx_thread_socket_fail.store(true);
+        return;
+    }
+}
 //------------------------------------------------------------------------------
 void dump(uint8_t *p, size_t size)
 {
