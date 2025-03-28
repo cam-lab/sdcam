30aa1ac5165d69c7785c627d0b8c90eee08389e2
hz <harry.zhurov@gmail.com>
2025-03-28 19:36:06 +0700

    prevent divion by zero

--- src/app/sdc_core.py
+++ src/app/sdc_core.py
@@ -361,8 +361,8 @@ class SdcCore(QObject):
                 self.nhisto.update(self.df)
 
                 if self._agc_ena:
-                    self.fgain = 1024/(self.ftop - self.forg)
                     self.forg, self.ftop = self.fbounds(self.nhisto.data, self.forg, self.ftop, 30)
+                    self.fgain = 1024/(self.ftop - self.forg + 1)
 
 
                 vframe.scale(self.ff, self.forg, self.fgain)
